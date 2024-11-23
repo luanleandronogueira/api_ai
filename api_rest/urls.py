a589d772488f7fc641c6e621f6928f8ae713857c
Luan Nogueira <luannogueira093@gmail.com>
2024-11-23 12:47:41 -0300

    API consultando o html legislativo

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_users, name='get_all_users'),
+    # path('', views.get_users, name='get_all_users'),
+    path('', views.chat_bot, name='chat_bot'),
     path('user/<str:nick>', views.get_by_nick, name='get_by_nick'),
     path('ai/<str:pergunta>', views.ask_question, name='ask_question'),
     path('template/<str:pergunta>', views.ask_question_template, name='ask_question_template'),
     path('search/<str:pergunta>', views.search_page, name='search_page'),
+    path('search_legislativo/<str:pergunta>', views.search_legislativo, name='search_legislativo'),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -15,7 +15,8 @@ api_key = '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'
 os.environ['GROQ_API_KEY'] = api_key
 chat = ChatGroq(model='llama3-groq-70b-8192-tool-use-preview')
 
-
+def chat_bot(request):
+    return render(request, 'chat_bot/chat.html')
 
 @api_view(['GET'])
 def get_users(request):
@@ -74,6 +75,30 @@ def search_page(request, pergunta):
     template = ChatPromptTemplate.from_messages([
         ("system", "Você é um assitente amigável chamado itAI e tem acesso as seguintes informações para dar as suas respostas: {documentos_informados}"),
         ("user", "{input}")
+    ])
+     
+    chain = template | chat
+    # resposta = chain.invoke({'documentos_informados' : documentos, 'input' : pergunta})
+    documentos = documentos[:4000]
+    try:
+        resposta = chain.invoke({'documentos_informados': documentos, 'input': pergunta})
+        return Response({'Resposta': resposta.content}, status=200)
+    except Exception as e:
+        return Response({'Erro': str(e)}, status=500)
+    
+    
+@api_view(['GET'])
+def search_legislativo(request, pergunta):
+    chat = ChatGroq(model='llama-3.2-11b-vision-preview')
+    loader = WebBaseLoader('https://bomjardim.pe.leg.br/API_pontomobile/legislativo.html')
+    dados_extraidos = loader.load()
+    documentos = ''
+    for doc in dados_extraidos:
+        documentos = documentos + doc.page_content
+
+    template = ChatPromptTemplate.from_messages([
+        ("system", "Você é um assistente chamado itAI, responsável por buscar informações dentro de documentos HTML. Sua tarefa é analisar as informações contidas no documento e fornecer um passo a passo claro e detalhado para ajudar o usuário a localizar as informações desejadas. Instruções: 1. Analise o documento HTML para identificar o conteúdo relevante, incluindo títulos, descrições e links. 2. Organize o conteúdo em um formato de passo a passo que oriente o usuário de forma clara sobre como encontrar as informações. - Cada passo deve ser uma instrução simples e direta. - Inclua o texto do link (se disponível) e o endereço literal do atributo `href`. - Adicione descrições úteis para contextualizar cada passo. Formato da Resposta: 1. Passo [Número]: [Descrição clara do que o usuário deve fazer]. - Link: [Texto do link]. Regras Adicionais: - Se não houver links ou informações claras no documento HTML, informe ao usuário que os dados não estão disponíveis no formato esperado. - Sempre siga a sequência lógica do documento HTML para apresentar os passos de forma organizada. - Use títulos e descrições dos elementos HTML como contexto adicional, caso estejam presentes. - Certifique-se de que os passos sejam fáceis de entender e executar. e tem acesso as seguintes informações para dar as suas respostas: {documentos_informados}"),
+    ("user", "{input}")
     ])
      
     chain = template | chat
