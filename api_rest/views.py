776d22e2c63b62460b369a0d851178a177e341ac
Luan Nogueira <luannogueira093@gmail.com>
2025-01-14 17:05:26 -0300

    Reformulação do Menu de pesquisa

--- api_rest/views.py
+++ api_rest/views.py
@@ -18,102 +18,102 @@ os.environ['GROQ_API_KEY'] = api_key
 chat = ChatGroq(model='llama3-groq-70b-8192-tool-use-preview') # modelo do LLama que está sendo usando
 
 # Temas que serão pesquisados
-Temas = {
-    "gestão": "temp/gestao.html",
-    "finanças": "temp/gestao.html",
-    "audiência pública": "temp/gestao.html",
-    "carta de serviços": "temp/gestao.html",
-    "estrutura organizacional": "temp/gestao.html",
-    "dados abertos": "temp/demais_atos.html",
-    "informações sigilosas": "temp/gestao.html",
-    "editais e concursos": "temp/gestao.html",
-    "consulta do duodécimo": "temp/receitas_despesas.html",
-    "receitas extraorçamentárias": "temp/receitas_despesas.html",
-    "despesas consolidadas": "temp/receitas_despesas.html",
-    "despesas detalhadas": "temp/receitas_despesas.html",
-    "orçamento público": "temp/receitas_despesas.html",
-    "controle interno": "temp/receitas_despesas.html",
-    "transparência": "temp/receitas_despesas.html",
-    "planejamento estratégico": "temp/gestao.html",
-    "responsabilidade fiscal": "temp/receitas_despesas.html",
-    "políticas públicas": "temp/demais_atos.html",
-    "relatórios financeiros": "temp/relatorios.html",
-    "licitações": "temp/gestao.html",
-    "contratos": "temp/gestao.html",
-    "recursos humanos": "temp/rh.html",
-    "salário": "temp/rh.html",
-    "pagamento": "temp/rh.html",
-    "despesas": "temp/receitas_despesas.html",
-    "pauta": "temp/gestao.html",
-    "atas": "temp/gestao.html",
-    "sessões": "temp/gestao.html",
-    "leis": "temp/demais_atos.html",
-    "lei": "temp/demais_atos.html",
-    "atricom": "temp/demais_atos.html",
-    "obras": "temp/demais_atos.html",
-    "decretos": "temp/demais_atos.html",
-    "somos": "temp/gestao.html",
-    "você": "temp/gestao.html",
-    "conhecimento": "temp/gestao.html",
-    "fale": "temp/gestao.html",
-}
+# Temas = {
+#     "gestão": "temp/gestao.html",
+#     "finanças": "temp/gestao.html",
+#     "audiência pública": "temp/gestao.html",
+#     "carta de serviços": "temp/gestao.html",
+#     "estrutura organizacional": "temp/gestao.html",
+#     "dados abertos": "temp/demais_atos.html",
+#     "informações sigilosas": "temp/gestao.html",
+#     "editais e concursos": "temp/gestao.html",
+#     "consulta do duodécimo": "temp/receitas_despesas.html",
+#     "receitas extraorçamentárias": "temp/receitas_despesas.html",
+#     "despesas consolidadas": "temp/receitas_despesas.html",
+#     "despesas detalhadas": "temp/receitas_despesas.html",
+#     "orçamento público": "temp/receitas_despesas.html",
+#     "controle interno": "temp/receitas_despesas.html",
+#     "transparência": "temp/receitas_despesas.html",
+#     "planejamento estratégico": "temp/gestao.html",
+#     "responsabilidade fiscal": "temp/receitas_despesas.html",
+#     "políticas públicas": "temp/demais_atos.html",
+#     "relatórios financeiros": "temp/relatorios.html",
+#     "licitações": "temp/gestao.html",
+#     "contratos": "temp/gestao.html",
+#     "recursos humanos": "temp/rh.html",
+#     "salário": "temp/rh.html",
+#     "pagamento": "temp/rh.html",
+#     "despesas": "temp/receitas_despesas.html",
+#     "pauta": "temp/gestao.html",
+#     "atas": "temp/gestao.html",
+#     "sessões": "temp/gestao.html",
+#     "leis": "temp/demais_atos.html",
+#     "lei": "temp/demais_atos.html",
+#     "atricom": "temp/demais_atos.html",
+#     "obras": "temp/demais_atos.html",
+#     "decretos": "temp/demais_atos.html",
+#     "somos": "temp/gestao.html",
+#     "você": "temp/gestao.html",
+#     "conhecimento": "temp/gestao.html",
+#     "fale": "temp/gestao.html",
+# }
 
 # Função que identifica o tema
-def identificar_tema(pergunta):
-    nlp = spacy.load("pt_core_news_sm")
-    doc = nlp(pergunta.lower())
+# def identificar_tema(pergunta):
+    # nlp = spacy.load("pt_core_news_sm")
+    # doc = nlp(pergunta.lower())
     
-    for token in doc:
-        if token.lemma_ in Temas:
-            return token.lemma_
-    return None
+    # for token in doc:
+    #     if token.lemma_ in Temas:
+    #         return token.lemma_
+    # return None
 
-@api_view(['GET'])
-def menu_completo(request, id, pergunta):
+# @api_view(['GET'])
+# def menu_completo(request, id, pergunta):
     # Inicializa o modelo
-    chat = ChatGroq(model='llama-3.2-11b-vision-preview')
+    # chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     
-    # Identifica o tema com base na pergunta
-    tema = identificar_tema(pergunta)
+    # # Identifica o tema com base na pergunta
+    # tema = identificar_tema(pergunta)
     
-    if tema and tema in Temas:
-        template_correto = Temas[tema]
-    else:
-        return Response({'Erro': 'Tema não identificado na pergunta.'}, status=400)
+    # if tema and tema in Temas:
+    #     template_correto = Temas[tema]
+    # else:
+    #     return Response({'Erro': 'Tema não identificado na pergunta.'}, status=400)
     
     
-    # Carrega o HTML baseado no endpoint identificado
-    html = render_to_string(template_correto, {'id': id})
+    # # Carrega o HTML baseado no endpoint identificado
+    # html = render_to_string(template_correto, {'id': id})
 
-    # Configura o template de prompt para o ChatGroq
-    template = ChatPromptTemplate.from_messages([
-        ("system", "Você é um assistente chamado itAI, responsável por buscar informações dentro de documentos HTML. Sua tarefa é analisar as informações contidas no documento e fornecer um passo a passo claro e detalhado para ajudar o usuário a localizar as informações desejadas. Instruções: 1. Analise o documento HTML para identificar o conteúdo relevante, incluindo títulos, descrições e links. 2. Organize o conteúdo em um formato de passo a passo que oriente o usuário de forma clara sobre como encontrar as informações. - Cada passo deve ser uma instrução simples e direta. - Inclua o texto do link (se disponível) e o endereço literal do atributo `href`. - Adicione descrições úteis para contextualizar cada passo. Formato da Resposta: 1. Passo [Número]: [Descrição clara do que o usuário deve fazer]. - Link: [Texto do link]. Regras Adicionais: - Se não houver links ou informações claras no documento HTML, informe ao usuário que os dados não estão disponíveis no formato esperado. - Sempre siga a sequência lógica do documento HTML para apresentar os passos de forma organizada. - Use títulos e descrições dos elementos HTML como contexto adicional, caso estejam presentes. - Certifique-se de que os passos sejam fáceis de entender e executar. e tem acesso as seguintes informações para dar as suas respostas: {template_correto}"),
-    ("user", "{input}")
-    ])
+    # # Configura o template de prompt para o ChatGroq
+    # template = ChatPromptTemplate.from_messages([
+    #     ("system", "Você é um assistente chamado itAI, responsável por buscar informações dentro de documentos HTML. Sua tarefa é analisar as informações contidas no documento e fornecer um passo a passo claro e detalhado para ajudar o usuário a localizar as informações desejadas. Instruções: 1. Analise o documento HTML para identificar o conteúdo relevante, incluindo títulos, descrições e links. 2. Organize o conteúdo em um formato de passo a passo que oriente o usuário de forma clara sobre como encontrar as informações. - Cada passo deve ser uma instrução simples e direta. - Inclua o texto do link (se disponível) e o endereço literal do atributo `href`. - Adicione descrições úteis para contextualizar cada passo. Formato da Resposta: 1. Passo [Número]: [Descrição clara do que o usuário deve fazer]. - Link: [Texto do link]. Regras Adicionais: - Se não houver links ou informações claras no documento HTML, informe ao usuário que os dados não estão disponíveis no formato esperado. - Sempre siga a sequência lógica do documento HTML para apresentar os passos de forma organizada. - Use títulos e descrições dos elementos HTML como contexto adicional, caso estejam presentes. - Certifique-se de que os passos sejam fáceis de entender e executar. e tem acesso as seguintes informações para dar as suas respostas: {template_correto}"),
+    # ("user", "{input}")
+    # ])
 
-    chain = template | chat
+    # chain = template | chat
 
-    # Limita o HTML para evitar excesso de caracteres
-    html = html[:4000]
+    # # Limita o HTML para evitar excesso de caracteres
+    # html = html[:4000]
 
-    # Tenta gerar a resposta
-    try:
-        resposta = chain.invoke({'template_correto': html, 'input': pergunta})
-        return Response({'Resposta': resposta.content}, status=200)
-    except Exception as e:
-        return Response({'Erro': str(e)}, status=500)
+    # # Tenta gerar a resposta
+    # try:
+    #     resposta = chain.invoke({'template_correto': html, 'input': pergunta})
+    #     return Response({'Resposta': resposta.content}, status=200)
+    # except Exception as e:
+    #     return Response({'Erro': str(e)}, status=500)
 
 #  Rota Index inicial
-def index(request):
-    return render(request, 'temp/index.html')
+# def index(request):
+    # return render(request, 'temp/index.html')
 
 # Acessar Qualquer Página e ver por ID
-def gestao_api(request, id):
+# def gestao_api(request, id):
     # Passar o ID como parte do contexto para o template
-    context = {
-        'id': id
-    }
-    return render(request, 'temp/gestao.html', context)
+    # context = {
+    #     'id': id
+    # }
+    # return render(request, 'temp/gestao.html', context)
 
 # Pergunta especifica sobre algum assunto relacionado ao tópico da pergunta
 @api_view(['GET'])
@@ -130,8 +130,8 @@ def pergunta_especifica(resquest, pergunta):
         return Response({'Resposta': resposta.content}, status=200)
 
 # Identificar sentido e palavra chave da pergunta
-@api_view(['GET'])
-def identifica_sentido(resquest, pergunta):
+# @api_view(['GET'])
+# def identifica_sentido(resquest, pergunta):
     chat = ChatGroq(model='llama3-groq-70b-8192-tool-use-preview')
     if resquest.method == 'GET':
         template = ChatPromptTemplate.from_messages(
@@ -153,7 +153,7 @@ def pergunta_aberta(resquest, pergunta):
 
 @api_view(['GET'])
 def menu_completo_api_id(request, id, pergunta):
-    chat = ChatGroq(model='llama3-groq-8b-8192-tool-use-preview')
+    chat = ChatGroq(model='llama-3.3-70b-versatile')
     context = {
         'id': id,
         'link': 'https://it-solucoes.com/transparenciaMunicipal/'
@@ -175,10 +175,55 @@ def menu_completo_api_id(request, id, pergunta):
         return Response({'Resposta': resposta.content}, status=200)
     except Exception as e:
         return Response({'Erro': str(e)}, status=500)
+    
+@api_view(['GET'])
+def menu_completo_api_id_pref(request, id, pergunta, sigla):
+    chat = ChatGroq(model='llama-3.3-70b-versatile')
+    context = ''  
+    html  = ''
+    if sigla == 0:
+        sigla = 'CM'
+        context = {
+        'id': id,
+        'link': 'http://portaltransparencia.app.br/v3',
+        'sigla': sigla
+        }
+        html = render_to_string('temp/menu_resumido.html', context)
+    elif sigla == 1:
+        sigla = 'PM'
+        context = {
+        'id': id,
+        'link': 'http://portaltransparencia.app.br/v3',
+        'sigla': sigla
+        }
+        html = render_to_string('temp/menu_resumido_pref.html', context)
+    elif sigla == 2:
+        sigla = 'FM'
+        context = {
+        'id': id,
+        'link': 'http://portaltransparencia.app.br/v3',
+        'sigla': sigla
+        }
+        html = render_to_string('temp/menu_resumido_fm.html', context)
+    # end if
+   
+    template = ChatPromptTemplate.from_messages([
+        ("system", "Você é um assistente chamado itAI, especializado em orientar os usuários a localizar informações em um portal com base em menus e links. Sua tarefa é analisar as perguntas recebidas, identificar o assunto principal e fornecer um passo a passo claro e detalhado para acessar as informações desejadas no portal. Instruções: 1. Leia atentamente a pergunta do usuário para identificar o assunto principal (ex.: remuneração, despesas, licitações). 2. Analise todas as seções e links disponíveis no documento HTML, priorizando aqueles mais relevantes ao tema da pergunta. 3. Organize sua resposta em um formato de passo a passo lógico e estruturado, descrevendo o caminho pelo menu e incluindo sempre o link da sessão idenficada. 4. A resposta deve ser clara, direta e formatada inteiramente em HTML, utilizando: - Uma <div> para conter o conteúdo. - Um <p> para cada passo, descrevendo as ações necessárias. - Os links em formato <a> com o texto clicável e o atributo href para a URL correspondente. 5. Garanta que a resposta seja fácil de compreender, sem menções técnicas como 'HTML', 'código', ou estruturas técnicas do documento. Formato da Resposta (em HTML): <div><p>1. Passo [Número]: [Descrição clara do que o usuário deve fazer].</p><p>- Link: <a href=\"[URL]\" title=\"[Descrição do link]\">[Texto do link]</a></p></div>. Regras: - Analise todas as seções do documento: Não limite sua busca à primeira seção encontrada; percorra todo o conteúdo. - Relacione diretamente a resposta ao tema da pergunta: Priorize títulos e descrições relevantes para encontrar a melhor correspondência. - Evite respostas genéricas ou desnecessárias: Não inclua links ou informações que não sejam diretamente úteis ao tema solicitado. - Caso não encontre informações relevantes: Informe que o dado solicitado não está disponível, formatando a mensagem dentro de uma <div> Sempre deve vir na resposta o link de acesso a sessão. E tem acesso as seguintes informações para dar as suas respostas: {documentos_informados}"),
+    ("user", "{input}")
+    ])
+    chain = template | chat
+
+    html = html[:20000]
+    
+    try:
+        resposta = chain.invoke({'documentos_informados': html, 'input': pergunta})
+        return Response({'Resposta': resposta.content}, status=200)
+    except Exception as e:
+        return Response({'Erro': str(e)}, status=500)
 
 # Menu Gestão
-@api_view(['GET'])
-def gestao_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def gestao_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/gestao.html', {'id':id})
 
@@ -198,8 +243,8 @@ def gestao_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
  
 # Menu Receitas e Despesas   
-@api_view(['GET'])
-def receitas_despesas_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def receitas_despesas_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/receitas_despesas.html', {'id':id})
 
@@ -219,8 +264,8 @@ def receitas_despesas_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
 
 # Menu Planejamento
-@api_view(['GET'])
-def planejamento_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def planejamento_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/planejamento.html', {'id':id})
 
@@ -240,8 +285,8 @@ def planejamento_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
     
 # Menu Relatórios    
-@api_view(['GET'])
-def relatorios_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def relatorios_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/relatorios.html', {'id':id})
 
@@ -261,8 +306,8 @@ def relatorios_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
 
 # Menu RH 
-@api_view(['GET'])
-def rh_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def rh_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/rh.html', {'id':id})
 
@@ -282,8 +327,8 @@ def rh_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
     
 # Menu Demais Atos   
-@api_view(['GET'])
-def demais_atos_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def demais_atos_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/demais_atos.html', {'id':id})
 
@@ -303,8 +348,8 @@ def demais_atos_api_id(request, id, pergunta):
         return Response({'Erro': str(e)}, status=500)
      
 # Menu Legislativo  
-@api_view(['GET'])
-def legislativo_api_id(request, id, pergunta):
+# @api_view(['GET'])
+# def legislativo_api_id(request, id, pergunta):
     chat = ChatGroq(model='llama-3.2-11b-vision-preview')
     html = render_to_string('temp/legislativo.html', {'id':id})
 
